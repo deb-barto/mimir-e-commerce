7fec5b535c5b6d1281543941208382704a504099
hana-bananaa <debartosiaki@gmail.com>
2021-07-24 20:07:11 -0300

    arrumando produtos adicionados no carrinho

--- JS/index.js
+++ JS/index.js
@@ -11,64 +11,66 @@ let products =[
     {
        name:'kigurumi Pokemon',
        tag:"kigurumi-1",
-       price: 110.10 ,
-       inCart: 0,
+       price: 110.00 ,
+       inCart: 1,
     },
     {
         name:'kigurumi Panda',
         tag:"kigurumi-2",
         price: 100.0 ,
-        inCart: 0,
+        inCart: 1,
      },
      {
         name:'kigurumi Stitch',
         tag:"kigurumi-3",
         price: 200.0 ,
-        inCart: 0,
+        inCart: 1,
      },
      {
         name:'kigurumi Jigglypuff',
         tag:"kigurumi-4",
         price: 250.0 ,
-        inCart: 0,
+        inCart: 1,
      },
      {
         name:'kigurumi Pokemon kid',
         tag:"kigurumi-5",
         price: 100.0 ,
-        inCart: 0,
+        inCart: 1,
      },
      {
         name:'kigurumi Panda kid',
         tag:"kigurumi-6",
         price: 100.00 ,
-        inCart: 0,
+        inCart: 1,
      },    {
         name:'kigurumi Unicorn Kid',
         tag:"kigurumi-7",
         price: 150.0 ,
-        inCart: 0,
+        inCart: 1,
      },
      {
         name:'kigurumi Jigglypuff',
         tag:"kigurumi-8",
         price: 100.0 ,
-        inCart: 0,
+        inCart: 1,
      },
 ]
 for (let i=0; i< carts.length; i++){
     carts[i] .addEventListener('click',()=>{
-        cartNumbers();
+        cartNumbers(products[i]);
     })
-    }
+}
 function onLoadCartNumbers() {
     let productNumbers = localStorage.getItem('cartNumbers');
+
     if(productNumbers){
         document.querySelector('.cart span').textContent = productNumbers ;
     }
 }
 
-function cartNumbers(){
+function cartNumbers(product){
+    console.log("The product is", product);
     let productNumbers = localStorage.getItem('cartNumbers');
    
     productNumbers = parseInt(productNumbers);
@@ -80,6 +82,26 @@ function cartNumbers(){
         localStorage.setItem('cartNumbers', 1);
         document.querySelector('.cart span').textContent = 1;
     }
+    setItems(product);
 }
+let cartItems = localStorage.getItem('ProductsInCart');
+cartItems = JSON.parse(cartItems);
+function setItems(product){
+    if (cartItems != null){
+        if(cartItems[product.tag] == undefined){
+            cartItems ={
+                ...cartItems,
+                [product.tag]: product
+            }
+        }
+        cartItems[product.tag].inCart +=  1;
+    } else {
+        product.inCart = 2;
+        cartItems = {
+            [product.tag]: product
+        }
+    }
 
-onLoadCartNumbers() 
+    localStorage.setItem("ProductsinCart", JSON.stringify(cartItems));
+}
+onLoadCartNumbers();
